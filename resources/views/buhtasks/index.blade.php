56be2c30a849defdc3014cc23575956b4e4fb747
iskender <iskender.neo@gmail.com>
2026-07-06 09:38:45 +0600

    fix company filter

--- resources/views/buhtasks/index.blade.php
+++ resources/views/buhtasks/index.blade.php
@@ -117,8 +117,8 @@
 
     {{-- Фильтры (только на вкладке «Список») --}}
     <div x-show="viewMode === 'list'" class="flex items-center gap-3 flex-wrap mb-4">
-        {{-- Компания --}}
-        <select x-show="clientOptions.length > 1" x-model="clientFilter"
+        {{-- Компания (показываем всегда, чтобы выбранная компания оставалась видна в фильтре) --}}
+        <select x-model="clientFilter"
                 class="px-3 py-2 border border-slate-200 rounded-xl text-sm bg-white focus:outline-none focus:ring-2 focus:ring-indigo-500/20 focus:border-indigo-500">
             <option value="all" x-text="'Все компании (' + tasks.length + ')'"></option>
             <template x-for="c in clientOptions" :key="c.id">
